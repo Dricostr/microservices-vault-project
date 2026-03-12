8896be726c522209370fd06c358944aa524afc5d
Dricostr <72656093+Dricostr@users.noreply.github.com>
2026-03-12 12:18:45 +0000

    changed the Main file to fix an issue

--- immobilier/booking-service/src/main/java/com/immobilier/booking/BookingServiceApplication.java
+++ immobilier/booking-service/src/main/java/com/immobilier/booking/BookingServiceApplication.java
@@ -2,8 +2,10 @@ package com.immobilier.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.immobilier.booking.repository")
 
 public class BookingServiceApplication {
     public static void main(String[] args) {
